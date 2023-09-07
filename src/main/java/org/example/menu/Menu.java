0d31ee5c18f68a3529a286d843dd4fe007881363
Admir Agic <112857246+rimdagic@users.noreply.github.com>
2023-09-07 14:39:01 +0200

    Added abstract Menu class

---
+++ src/main/java/org/example/menu/Menu.java
@@ -0,0 +1,37 @@
+package org.example.menu;
+
+import java.util.List;
+import java.util.Scanner;
+
+public abstract class Menu implements MenuState {
+
+    protected String name;
+    protected List<MenuOption> menuOptions;
+    public Menu(String name){
+        this.name = name;
+    }
+
+
+    @Override
+    public void execute() {
+        displayMenuOptions();
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+        for( MenuOption menuOption : menuOptions){
+            if(menuOption.getOptionNumber() == option)
+                menuOption.run();
+    }
+}
+
+    private void displayMenuOptions() {
+
+        String menuText = name;
+        for(MenuOption menuOption : menuOptions){
+            menuText = menuText.concat(
+                    "\n" + menuOption.getOptionNumber()
+                            + " " + menuOption.getText()
+            );
+        }
+        System.out.println(menuText);
+    }
+}
